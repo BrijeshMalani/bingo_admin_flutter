e40948e5935d0d43fd69205944d049fd6986ab36
BrijeshMalani <72848902+BrijeshMalani@users.noreply.github.com>
2025-08-13 18:06:18 +0530

    correction

--- build/web/flutter_bootstrap.js
+++ build/web/flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1515177391"
+    serviceWorkerVersion: "3965936558"
   }
 });
